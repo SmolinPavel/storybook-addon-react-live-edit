951b5cc3785d2b8298260b13ea0ee48b28137ff4
Pavel Smolin <isingwithaz@gmail.com>
2019-06-21 16:37:55 +0300

    Add fix for custom theme usage

--- example/index.js
+++ example/index.js
@@ -1,15 +1,28 @@
 // @flow
-import * as React from 'react';
-import {storiesOf, setAddon} from '@storybook/react';
-import LiveEdit, { withLiveEditScope } from '../dist';
+import * as React from "react";
+import { storiesOf, setAddon } from "@storybook/react";
+import LiveEdit, { withLiveEditScope } from "../dist";
 
 setAddon(LiveEdit);
 
-storiesOf('Example', module)
-    .addDecorator(withLiveEditScope({ React, scopeTest: 'Passed!' }))
-    .addLiveSource('Demo', `return <div style={{ padding: '10px', border: '1px solid red', margin: 10 }}>demo</div>;`)
-    .addLiveSource('Demo 2', `return <div style={{ padding: '10px', border: '3px solid blue', margin: 10 }}>{scopeTest}</div>`, {
-        localScopeTest: 'Passed as well!',
-        Test: ({ text }) => <b style={{ fontSize: 'xx-large', color: 'orange'}}>{text}</b>
-    })
-    .add('Static source', () => <div />);
+const Test = () => {
+  return <h1>pasha test</h1>;
+};
+
+storiesOf("Example", module)
+  .addDecorator(withLiveEditScope({ React, scopeTest: "Passed!", Test }))
+  .addLiveSource(
+    "Demo",
+    `<Test />`
+  )
+  .addLiveSource(
+    "Demo 2",
+    `<div style={{ padding: '10px', border: '3px solid blue', margin: 10 }}>{scopeTest}</div>`,
+    {
+      localScopeTest: "Passed as well!",
+      Test: ({ text }) => (
+        <b style={{ fontSize: "xx-large", color: "orange" }}>{text}</b>
+      )
+    }
+  )
+  .add("Static source", () => <h1>Pavel</h1>);
--- src/component/LiveEditor/index.js
+++ src/component/LiveEditor/index.js
@@ -1,135 +1,162 @@
 // @flow
-import * as React from 'react';
-import CodeMirror from 'codemirror';
-import { event } from '../../constants';
-import CodeMirrorStyle, * as themes from './styles';
+import * as React from "react";
+import CodeMirror from "codemirror";
+import { event } from "../../constants";
+import CodeMirrorStyle, * as themes from "./styles";
 
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/mode/jsx/jsx';
-import { themeNameToVarName } from './utils';
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/jsx/jsx";
+import { themeNameToVarName } from "./utils";
 
 type LiveEditorProps = {
-    channel: any,
-    api: any,
-    theme?: ?string,
-    active: boolean
+  channel: any,
+  api: any,
+  theme?: ?string,
+  active: boolean
 };
 
 type LiveEditorState = {
-    code: ?string,
-    theme: ?string
+  code: ?string,
+  theme: ?string
 };
 
-export default class LiveEditor extends React.Component<LiveEditorProps, LiveEditorState> {
-    state = {
-        code: null,
-        theme: this.props.theme
-    };
+export default class LiveEditor extends React.Component<
+  LiveEditorProps,
+  LiveEditorState
+> {
+  state = {
+    code: null,
+    theme: this.props.theme
+  };
 
-    codeMirrorRef: ?HTMLElement;
-    codeMirror: *;
+  codeMirrorRef: ?HTMLElement;
+  codeMirror: *;
 
-    constructor(...args: *) {
-        super(...args);
+  constructor(...args: *) {
+    super(...args);
 
-        // $FlowIgnore
-        this.loadSource = this.loadSource.bind(this);
-        // $FlowIgnore
-        this.loadOptions = this.loadOptions.bind(this);
-        // $FlowIgnore
-        this.onSourceChange = this.onSourceChange.bind(this);
-        // $FlowIgnore
-        this.setCodeMirrorRef = this.setCodeMirrorRef.bind(this);
-        // $FlowIgnore
-        this.codemirrorValueChanged = this.codemirrorValueChanged.bind(this);
+    // $FlowIgnore
+    this.loadSource = this.loadSource.bind(this);
+    // $FlowIgnore
+    this.loadOptions = this.loadOptions.bind(this);
+    // $FlowIgnore
+    this.onSourceChange = this.onSourceChange.bind(this);
+    // $FlowIgnore
+    this.setCodeMirrorRef = this.setCodeMirrorRef.bind(this);
+    // $FlowIgnore
+    this.codemirrorValueChanged = this.codemirrorValueChanged.bind(this);
+  }
+
+  loadSource(code: string) {
+    this.setState({ code });
+  }
+
+  loadOptions(options: { theme: ?string }) {
+    this.setState({ theme: options.theme });
+  }
+
+  onSourceChange(code: string) {
+    this.props.channel.emit(event.UpdateSource, code);
+  }
+
+  componentDidMount() {
+    this.props.channel.on(event.LoadSource, this.loadSource);
+    this.props.channel.on(event.SyncOptions, this.loadOptions);
+  }
+
+  componentWillUnmount() {
+    this.props.channel.removeListener(event.LoadSource, this.loadSource);
+    this.props.channel.removeListener(event.SyncOptions, this.loadOptions);
+  }
+
+  componentDidUpdate() {
+    const preparedTheme =
+      typeof this.state.theme === "string" ? this.state.theme : "ctm";
+    if (this.state.code !== null && this.props.active) {
+      const theme =
+        themeNameToVarName(preparedTheme) in themes ? preparedTheme : "default";
+      if (!this.codeMirror) {
+        this.codeMirror = CodeMirror(this.codeMirrorRef, {
+          mode: "jsx",
+          theme
+        });
+        this.codeMirror.on("change", this.codemirrorValueChanged);
+      } else {
+        this.codeMirror.setOption("theme", theme);
+      }
+      // $FlowIgnore
+      this.codeMirror.setValue(this.state.code);
+    } else {
+      this.codeMirror = null;
+    }
+  }
+
+  codemirrorValueChanged(doc: *, change: *) {
+    if (change.origin !== "setValue") {
+      this.onSourceChange(doc.getValue());
+    }
+  }
+
+  setCodeMirrorRef(el: ?HTMLElement) {
+    this.codeMirrorRef = el;
+  }
+
+  render(): any {
+    if (!this.props.active) {
+      return null;
     }
 
-    loadSource(code: string) {
-        this.setState({ code });
+    if (this.state.code === null) {
+      return (
+        <div
+          style={{
+            width: "100%",
+            textAlign: "center",
+            padding: "20px",
+            color: "gray",
+            textTransform: "uppercase"
+          }}
+        >
+          Editor unavailable
+        </div>
+      );
     }
 
-    loadOptions(options: { theme: ?string }) {
-        this.setState({ theme: options.theme });
-    }
-
-    onSourceChange(code: string) {
-        this.props.channel.emit(event.UpdateSource, code);
-    }
-
-    componentDidMount() {
-        this.props.channel.on(event.LoadSource, this.loadSource);
-        this.props.channel.on(event.SyncOptions, this.loadOptions);
-    }
-
-    componentWillUnmount() {
-        this.props.channel.removeListener(event.LoadSource, this.loadSource);
-        this.props.channel.removeListener(event.SyncOptions, this.loadOptions);
-    }
-
-    componentDidUpdate() {
-        if (this.state.code !== null && this.props.active) {
-            const theme = themeNameToVarName(this.state.theme) in themes ? this.state.theme : 'default';
-            if (!this.codeMirror) {
-                this.codeMirror = CodeMirror(this.codeMirrorRef, { mode: 'jsx', theme });
-                this.codeMirror.on('change', this.codemirrorValueChanged);
-            } else {
-                this.codeMirror.setOption('theme', theme);
-            }
-            // $FlowIgnore
-            this.codeMirror.setValue(this.state.code);
-        } else {
-            this.codeMirror = null;
-        }
-    }
-
-    codemirrorValueChanged(doc: *, change: *) {
-        if (change.origin !== 'setValue') {
-            this.onSourceChange(doc.getValue());
-        }
-    }
-
-    setCodeMirrorRef(el: ?HTMLElement) {
-        this.codeMirrorRef = el;
-    }
-
-    render(): any {
-        if (!this.props.active) {
-            return null;
-        }
-
-        if (this.state.code === null) {
-            return (
-                <div
-                    style={{
-                        width: '100%',
-                        textAlign: 'center',
-                        padding: '20px',
-                        color: 'gray',
-                        textTransform: 'uppercase'
-                    }}
-                >
-                    Editor unavailable
-                </div>
-            );
-        }
-
-        const themeVarName = themeNameToVarName(this.state.theme);
-        return (
-            <div key={themeVarName} style={{ width: '100%', display: 'flex', flex: '1 1', minHeight: '100%' }}>
-                {themeVarName in themes && <style dangerouslySetInnerHTML={{ __html: themes[themeVarName] }} />}
-                <style dangerouslySetInnerHTML={{ __html: CodeMirrorStyle }} />
-                <style
-                    dangerouslySetInnerHTML={{
-                        __html: `
+    const themeVarName = 'default'; // themeNameToVarName(this.state.theme);
+    return (
+      <div
+        key={themeVarName}
+        style={{
+          width: "100%",
+          display: "flex",
+          flex: "1 1",
+          minHeight: "100%"
+        }}
+      >
+        {themeVarName in themes && (
+          <style dangerouslySetInnerHTML={{ __html: themes[themeVarName] }} />
+        )}
+        <style dangerouslySetInnerHTML={{ __html: CodeMirrorStyle }} />
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
                         div.CodeMirror {
                             width: 100%;
                             height: initial;
                         }
                     `
-                    }}
-                />
-                <div style={{ minWidth: '100%', minHeight: '100%', display: 'flex', flex: '1 1' }} ref={this.setCodeMirrorRef} />
-            </div>
-        );
-    }
+          }}
+        />
+        <div
+          style={{
+            minWidth: "100%",
+            minHeight: "100%",
+            display: "flex",
+            flex: "1 1"
+          }}
+          ref={this.setCodeMirrorRef}
+        />
+      </div>
+    );
+  }
 }
